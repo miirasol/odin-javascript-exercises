c38b500b7bda07e9e2462b63e85247df7dd75c9a
miirasol <110359854+miirasol@users.noreply.github.com>
2025-07-01 18:49:48 +0800

    Solve 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = (string) => {
+    let reverse = string.split('');
 
+    reverse.reverse();
+
+    return reverse.join('');
 };
 
 // Do not edit below this line
