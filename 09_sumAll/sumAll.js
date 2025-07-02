a3b08b3fc39c95e09bab59cb6ec754c6eaf24860
miirasol <110359854+miirasol@users.noreply.github.com>
2025-07-02 10:41:31 +0800

    Solve 09_sumALl

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+	if (
+		a < 0 ||
+		b < 0 ||
+		isNaN(a) ||
+		isNaN(b) ||
+		!Number.isInteger(a) ||
+		!Number.isInteger(b)
+	) {
+		return "ERROR";
+	}
 
+	let sum = 0;
+	let temp = 0;
+
+	if (a > b) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+
+	for (let i = a; i <= b; i++) {
+		sum += i;
+	}
+
+    return sum;
 };
 
 // Do not edit below this line
