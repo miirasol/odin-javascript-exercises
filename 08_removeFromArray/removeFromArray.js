7e5b3f8fbdbbdd47cbf613b078a9e8aece0e1b83
miirasol <110359854+miirasol@users.noreply.github.com>
2025-07-02 10:26:49 +0800

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...nums) => {
+    // let i = 0;
+    // let j = 0;
+
+    // while (i < nums.length) {
+    //     while (j < array.length) {
+    //         if (array[j] === nums[i]) {
+    //             array.splice(j, 1);
+    //         } else j++;
+    //     }
+    //     i++;
+    //     j = 0;
+    // }
+
+    // return array;
+
+    return array.filter((item) => !nums.includes(item));
 };
 
 // Do not edit below this line
