6f85cb2ff5668c6a7e6da7873b0be2002c4a3d29
miirasol <110359854+miirasol@users.noreply.github.com>
2025-07-12 23:09:22 +0800

    Solve 13_palindromes.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	const punctuations = "!?., ";
+	string = string.toLowerCase();
+	let cleanText = "";
 
+	const leftChar = (i) => cleanText.charAt(i);
+
+	const rightChar = (i) => cleanText.charAt(cleanText.length - 1 - i);
+
+	for (let i = 0; i < string.length; i++) {
+		const char = string[i];
+		if (punctuations.indexOf(char) == -1) {
+			cleanText += char;
+		}
+	}
+
+	for (let i = 0; i < cleanText.length / 2; i++) {
+		if (leftChar(i) != rightChar(i)) {
+			return false;
+		}
+	}
+
+	return true;
 };
 
 // Do not edit below this line
